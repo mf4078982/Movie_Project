9db155929fd33619bc60e11389ecc5aa69ede09f
mf4078982 <mf4078982@gmail.com>
2024-09-10 06:06:34 -0700

    change the show page

--- movie/movie_app/urls.py
+++ movie/movie_app/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('edit/<int:id>/', views.edit, name='edit'),
     path('delete_movie/<int:id>/', views.delete_movie, name='delete_movie'),
     path('register/', views.register, name='register'),
-    # path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    # path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
     path('', views.movieapp_view, name='movieapp'),
     
 ]
